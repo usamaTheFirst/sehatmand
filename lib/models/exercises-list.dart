7a76af4d2e75715424b76b5c5d2297849e61830d
Usama <72226908+usamaTheFirst@users.noreply.github.com>
2022-07-18 12:52:16 +0500

    minor changes

---
+++ lib/models/exercises-list.dart
@@ -0,0 +1,36 @@
+import '../widgets/exercise_widget.dart';
+
+final exercises_list = [
+  ExerciseWidget(
+    title: "Running",
+    image:
+        "https://images.everydayhealth.com/images/how-to-start-working-out-again-derailed-from-covid-1440x810.jpg",
+    description:
+        'Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet are above the ground (though there are exceptions)',
+    calories: 100,
+  ),
+  ExerciseWidget(
+    title: "Running",
+    image:
+        "https://images.everydayhealth.com/images/how-to-start-working-out-again-derailed-from-covid-1440x810.jpg",
+    description:
+        'Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet are above the ground (though there are exceptions)',
+    calories: 100,
+  ),
+  ExerciseWidget(
+    title: "Running",
+    image:
+        "https://images.everydayhealth.com/images/how-to-start-working-out-again-derailed-from-covid-1440x810.jpg",
+    description:
+        'Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet are above the ground (though there are exceptions)',
+    calories: 100,
+  ),
+  ExerciseWidget(
+    title: "Running",
+    image:
+        "https://images.everydayhealth.com/images/how-to-start-working-out-again-derailed-from-covid-1440x810.jpg",
+    description:
+        'Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet are above the ground (though there are exceptions)',
+    calories: 100,
+  ),
+];
