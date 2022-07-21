b08ceee4e64cda1bf60ffecea5ccb24365573356
Usama <72226908+usamaTheFirst@users.noreply.github.com>
2022-07-21 16:39:10 +0500

    Calorie count back end integrated

---
+++ lib/models/food.dart
@@ -0,0 +1,37 @@
+class Food {
+  final String name;
+  final double calories;
+
+  Food(this.name, this.calories);
+}
+
+final food_list = [
+  Food("Apple", 52),
+  Food("Bread", 67),
+  Food("Cereal", 110),
+  Food("Chocolate Milk", 208),
+  Food("Yougurt", 220),
+  Food("Dates", 228),
+  Food("Orange Juice", 112),
+  Food("PineApple", 77),
+  Food("Carrot", 31),
+  Food("Biscuit", 103),
+  Food("Noodles", 159),
+  Food("Pizza", 290),
+  Food("Rice", 223),
+  Food("Mutton", 190),
+  Food("Beef", 200),
+  Food("egg", 254),
+  Food("Chicken Sandwich", 60),
+  Food("Milk Shake", 112),
+  Food("Pumpkin", 53),
+  Food("Almonds", 54),
+  Food("Banana", 105),
+  Food("Banana", 105),
+  Food("Banana", 105),
+  Food("Banana", 105),
+  Food("Banana", 105),
+  Food("Banana", 105),
+  Food("Banana", 105),
+  Food("Banana", 105),
+];
